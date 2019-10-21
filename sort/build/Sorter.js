34db91657e721f5aacc91fff439035258518278e
ipankaj <pankaj.garg@infoedge.com>
2019-10-21 14:45:23 +0530

    refactoring

--- sort/build/Sorter.js
+++ sort/build/Sorter.js
@@ -2,16 +2,14 @@
 //import { NumberCollection } from "./NumberCollection";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-        this.collection = collection;
+    function Sorter() {
     }
     Sorter.prototype.sort = function () {
-        var length = this.collection.length;
+        var length = this.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                    this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
--- sort/build/index.js
+++ sort/build/index.js
@@ -1,14 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
-//import { NumberCollection } from "./NumberCollection";
-var CharactersCollection_1 = require("./CharactersCollection");
-// const numbersCollection = new NumberCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection);
-var charactersCollection = new CharactersCollection_1.CharactersCollection("Xaayb");
-var sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
-//
-console.log(charactersCollection);
+var NumberCollection_1 = require("./NumberCollection");
+var numbersCollection = new NumberCollection_1.NumberCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection);
+function fun(obj) {
+    console.log(obj);
+}
+var obj = { a: 1, b: 1 };
+fun(obj);
